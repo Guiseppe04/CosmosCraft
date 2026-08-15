7a0456d8cc0c88f3dcce48ac7ea84867a94d483a
mark-apk <fuentesmarkjasper.pdm@gmail.com>
2026-08-15 21:56:42 +0800

    Revert "Fixed"
    This reverts commit 28edcd1e557f62d142e9d63652b012a7fef2928f.

--- client/src/app/context/CartContext.jsx
+++ client/src/app/context/CartContext.jsx
@@ -64,19 +64,7 @@ export function CartProvider({ children }) {
           quantity: item.quantity,
           type: 'product'
         }))
-
-        const mergedCart = []
-        const cartMap = new Map()
-        for (const item of mappedCart) {
-          if (cartMap.has(item.id)) {
-            const existing = cartMap.get(item.id)
-            existing.quantity = existing.quantity + item.quantity
-          } else {
-            cartMap.set(item.id, item)
-            mergedCart.push(item)
-          }
-        }
-        setCart(mergedCart)
+        setCart(mappedCart)
       } else {
         setCart([])
       }
